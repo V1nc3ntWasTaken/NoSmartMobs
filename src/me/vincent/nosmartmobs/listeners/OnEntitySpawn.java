adbbf7cd7060eec7454d2096a9896ce24199077f
V1nc3ntWasTaken <79623093+V1nc3ntWasTaken@users.noreply.github.com>
2021-02-25 04:41:42 -0600

    Updated OnEntitySpawn.java
    No longer uses EntitySpawnEvent, and now uses CreatureSpawnEvent

--- src/me/vincent/nosmartmobs/listeners/OnEntitySpawn.java
+++ src/me/vincent/nosmartmobs/listeners/OnEntitySpawn.java
@@ -1,13 +1,11 @@
 package me.vincent.nosmartmobs.listeners;
 
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 
 import me.vincent.nosmartmobs.Main;
 
@@ -24,50 +22,47 @@ public class OnEntitySpawn implements Listener {
 	}
 	
 	@EventHandler
-	public void onSpawn(EntitySpawnEvent event) {
+	public void onSpawn(CreatureSpawnEvent event) {
 		
-		EntityType entity = (EntityType) event.getEntity().getType();
+		LivingEntity entity = (LivingEntity) event.getEntity();
 
-		if (entity.equals(EntityType.AREA_EFFECT_CLOUD)) return;
-		if (entity.equals(EntityType.ARMOR_STAND)) return;
-		if (entity.equals(EntityType.ARROW)) return;
-		if (entity.equals(EntityType.BOAT)) return;
-		if (entity.equals(EntityType.DRAGON_FIREBALL)) return;
-		if (entity.equals(EntityType.DROPPED_ITEM)) return;
-		if (entity.equals(EntityType.EGG)) return;
-		if (entity.equals(EntityType.ENDER_CRYSTAL)) return;
-		if (entity.equals(EntityType.ENDER_PEARL)) return;
-		if (entity.equals(EntityType.EXPERIENCE_ORB)) return;
-		if (entity.equals(EntityType.FIREBALL)) return;
-		if (entity.equals(EntityType.FIREWORK)) return;
-		if (entity.equals(EntityType.FISHING_HOOK)) return;
-		if (entity.equals(EntityType.ITEM_FRAME)) return;
-		if (entity.equals(EntityType.ITEM_FRAME)) return;
-		if (entity.equals(EntityType.LEASH_HITCH)) return;
-		if (entity.equals(EntityType.LIGHTNING)) return;
-		if (entity.equals(EntityType.LLAMA_SPIT)) return;
-		if (entity.equals(EntityType.MINECART)) return;
-		if (entity.equals(EntityType.MINECART_CHEST)) return;
-		if (entity.equals(EntityType.MINECART_COMMAND)) return;
-		if (entity.equals(EntityType.MINECART_FURNACE)) return;
-		if (entity.equals(EntityType.MINECART_HOPPER)) return;
-		if (entity.equals(EntityType.MINECART_MOB_SPAWNER)) return;
-		if (entity.equals(EntityType.MINECART_TNT)) return;
-		if (entity.equals(EntityType.PAINTING)) return;
-		if (entity.equals(EntityType.PLAYER)) return;
-		if (entity.equals(EntityType.PRIMED_TNT)) return;
-		if (entity.equals(EntityType.SHULKER_BULLET)) return;
-		if (entity.equals(EntityType.SMALL_FIREBALL)) return;
-		if (entity.equals(EntityType.SPECTRAL_ARROW)) return;
-		if (entity.equals(EntityType.SPLASH_POTION)) return;
-		if (entity.equals(EntityType.THROWN_EXP_BOTTLE)) return;
-		if (entity.equals(EntityType.TRIDENT)) return;
-		if (entity.equals(EntityType.UNKNOWN)) return;
+		if (entity.getType().equals(EntityType.AREA_EFFECT_CLOUD)) return;
+		if (entity.getType().equals(EntityType.ARMOR_STAND)) return;
+		if (entity.getType().equals(EntityType.ARROW)) return;
+		if (entity.getType().equals(EntityType.BOAT)) return;
+		if (entity.getType().equals(EntityType.DRAGON_FIREBALL)) return;
+		if (entity.getType().equals(EntityType.DROPPED_ITEM)) return;
+		if (entity.getType().equals(EntityType.EGG)) return;
+		if (entity.getType().equals(EntityType.ENDER_CRYSTAL)) return;
+		if (entity.getType().equals(EntityType.ENDER_PEARL)) return;
+		if (entity.getType().equals(EntityType.EXPERIENCE_ORB)) return;
+		if (entity.getType().equals(EntityType.FIREBALL)) return;
+		if (entity.getType().equals(EntityType.FIREWORK)) return;
+		if (entity.getType().equals(EntityType.FISHING_HOOK)) return;
+		if (entity.getType().equals(EntityType.ITEM_FRAME)) return;
+		if (entity.getType().equals(EntityType.ITEM_FRAME)) return;
+		if (entity.getType().equals(EntityType.LEASH_HITCH)) return;
+		if (entity.getType().equals(EntityType.LIGHTNING)) return;
+		if (entity.getType().equals(EntityType.LLAMA_SPIT)) return;
+		if (entity.getType().equals(EntityType.MINECART)) return;
+		if (entity.getType().equals(EntityType.MINECART_CHEST)) return;
+		if (entity.getType().equals(EntityType.MINECART_COMMAND)) return;
+		if (entity.getType().equals(EntityType.MINECART_FURNACE)) return;
+		if (entity.getType().equals(EntityType.MINECART_HOPPER)) return;
+		if (entity.getType().equals(EntityType.MINECART_MOB_SPAWNER)) return;
+		if (entity.getType().equals(EntityType.MINECART_TNT)) return;
+		if (entity.getType().equals(EntityType.PAINTING)) return;
+		if (entity.getType().equals(EntityType.PLAYER)) return;
+		if (entity.getType().equals(EntityType.PRIMED_TNT)) return;
+		if (entity.getType().equals(EntityType.SHULKER_BULLET)) return;
+		if (entity.getType().equals(EntityType.SMALL_FIREBALL)) return;
+		if (entity.getType().equals(EntityType.SPECTRAL_ARROW)) return;
+		if (entity.getType().equals(EntityType.SPLASH_POTION)) return;
+		if (entity.getType().equals(EntityType.THROWN_EXP_BOTTLE)) return;
+		if (entity.getType().equals(EntityType.TRIDENT)) return;
+		if (entity.getType().equals(EntityType.UNKNOWN)) return;
 			
-		UUID temp = (UUID) event.getEntity().getUniqueId();
-		LivingEntity entityL = (LivingEntity) Bukkit.getEntity(temp);
-		
-		entityL.setAI(false);
+		entity.setAI(false);
 		
 	}
 	
